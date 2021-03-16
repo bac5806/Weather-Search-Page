11738a09c51d5bb99fc6e0206223bd70e7c7980d
Broc C <bac5806@gmail.com>
2021-03-16 15:47:53 -0400

    finished commenting code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,9 @@ icon.className = "fa fa-search fa-lg";
 // apend icong to buttons
 btnEl.append(icon);
 
+            /* this section is for when the user uses 
+            the search bar to look up weather for a city */
+
 // button event listener
 btnEl.addEventListener("click", function(event) {
     event.preventDefault();
@@ -36,18 +39,23 @@ btnEl.addEventListener("click", function(event) {
         // if city is different, display to page and store in local
         if (isDifCity) {
 
+            // crear button element and assign className
             var cityBtnEl = document.createElement("button");
             cityBtnEl.className = "cityName";
             cityBtnEl.textContent = getCity;
+            // append button to cityNameContiner
             cityNameContEl.append(cityBtnEl);
 
+            // add city name to array
             cityNameArr.push(getCity);
+            // store in local
             localStorage.setItem("cityNames", JSON.stringify(cityNameArr));
         }
         
         // concat the api call with input from user along with api key
         var apiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + getCity + "&units=imperial" + "&appid=" + key;
 
+        // create html elements for displaying weather info
         var todaysForecastEl = document.createElement("div");
         todaysForecastEl.className = "todaysForecast";
         var getDate = moment().format("DD/MM/YYYY");
@@ -57,6 +65,7 @@ btnEl.addEventListener("click", function(event) {
         var windSpeedEl = document.createElement("p");
         var uvEl = document.createElement("p");
 
+        // empty string used for displaying UV
         var getUv = "";
 
         // fetch link
@@ -68,14 +77,17 @@ btnEl.addEventListener("click", function(event) {
             .then(function (data) {
                 console.log(data);
             
+                // set content of html elements 
                 cityName.textContent = (data.name + " " + "(" + getDate + ")");
                 tempEl.innerHTML = "Temperature: " + data.main.temp + " " + "&#x2109";
                 humidityEl.innerHTML = "Humidity: " + data.main.humidity + "\%";
                 windSpeedEl.innerHTML = "Wind Speed: " + data.wind.speed + " MPH";
                 
+                // get latitude and longitude for city to get the UV 
                 var lati = data.coord.lat;
                 var long = data.coord.lon;
 
+                // fetch api for UV info
                 getUv = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lati + "&lon=" + long + "&appid=" + key;
                 fetch(getUv, {
                 })
@@ -83,14 +95,17 @@ btnEl.addEventListener("click", function(event) {
                         return response1.json();
                     })
                     .then(function (data1) {
+                        // set UV info
                         uvEl.innerHTML = "UV Index: " + data1.value;
                 });  
 
+                // creat html elements for 5 day forecast
                 var fiveDayH2El = document.createElement("h2");
                 fiveDayH2El.textContent = "5 Day Forecast";
                 fiveDayH2El.className = "fiveDayH2";
                 weatherContEl.append(fiveDayH2El);
 
+                // fetch 5 day foracst api
                 var getFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + getCity + "&units=imperial" + "&appid=" + key;
                 fetch(getFiveDay, {
                 })
@@ -100,44 +115,59 @@ btnEl.addEventListener("click", function(event) {
                     .then(function (data2) {
                         console.log(data2);
 
+                        // crate div element and set className
                         var fiveDayCont = document.createElement("div");
                         fiveDayCont.className = "fiveDayForecast";
                         
+                        // loop through 5 day forecast response length, and set info for displaying
                         var addDay = 1;
                         // var fiveDayDate = moment().add("d", addDay).format("M/DD/YYYY");
                         for (var i = 0; i < 40; i+=8) {
+                            // create div container element for holding info for each day
                             var dayContEl = document.createElement("div");
                             dayContEl.className = "dayCont";
 
+                            // create h3 element and set content using moment
                             var dateEl = document.createElement("h3");
                             dateEl.innerHTML = moment().add(addDay, "d").format("M/DD/YYYY");
 
+                            // create p element and set its content to temp infi
                             var fiveDayTemp = document.createElement("p");
                             fiveDayTemp.innerHTML = "Temp: " + data2.list[i].main.temp + " " + "&#x2109";
 
+                            // create p element and set its content to humidity
                             var fiveDayHumidity = document.createElement("p");
                             fiveDayHumidity.innerHTML = "Humidity: " + data2.list[i].main.humidity + "\%";
 
+                            // append created elements 
                             dayContEl.append(dateEl, fiveDayTemp, fiveDayHumidity);
                             fiveDayCont.append(dayContEl);
+                            // increment day variable
                             addDay += 1;
                         }
                         weatherContEl.append(fiveDayCont);
                 });  
         }); 
 
+        // append all elements to the main content container
         todaysForecastEl.append(cityName, tempEl, humidityEl, windSpeedEl, uvEl);
         weatherContEl.append(todaysForecastEl);
+        // reset input value
         inputEl.value = "";
     }
 });
 
+            /* This section is for when the user selects the 
+            button of a city that was previously searched for */
+
+// created event listeners for all stored buttons
 function cityButtons(){
 var getButtons = document.getElementsByClassName("cityName");
 for (var i = 0; i < getButtons.length; ++i) {    
     getButtons[i].addEventListener("click", function(event) {
         event.preventDefault();
 
+        // reset innerHTML for weather container
         weatherContEl.innerHTML = "";
 
         // get input text
@@ -146,6 +176,7 @@ for (var i = 0; i < getButtons.length; ++i) {
         // concat the api call with input from user along with api key
         var apiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + getCity + "&units=imperial" + "&appid=" + key;
 
+        // create html elemetns 
         var todaysForecastEl = document.createElement("div");
         todaysForecastEl.className = "todaysForecast";
         var getDate = moment().format("DD/MM/YYYY");
@@ -155,6 +186,7 @@ for (var i = 0; i < getButtons.length; ++i) {
         var windSpeedEl = document.createElement("p");
         var uvEl = document.createElement("p");
 
+        // empty string used for getting UV info
         var getUv = "";
 
         // fetch link
@@ -166,14 +198,17 @@ for (var i = 0; i < getButtons.length; ++i) {
             .then(function (data) {
                 console.log(data);
             
+                // set content of created html elements
                 cityName.textContent = (data.name + " " + "(" + getDate + ")");
                 tempEl.innerHTML = "Temperature: " + data.main.temp + " " + "&#x2109";
                 humidityEl.innerHTML = "Humidity: " + data.main.humidity + "\%";
                 windSpeedEl.innerHTML = "Wind Speed: " + data.wind.speed + " MPH";
                 
+                // get latitude and longitude data for UV
                 var lati = data.coord.lat;
                 var long = data.coord.lon;
 
+                // fetch api for UV
                 getUv = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lati + "&lon=" + long + "&appid=" + key;
                 fetch(getUv, {
                 })
@@ -183,12 +218,14 @@ for (var i = 0; i < getButtons.length; ++i) {
                     .then(function (data1) {
                         uvEl.innerHTML = "UV Index: " + data1.value;
                 });  
-
+            
+        // create html elements for displaying 5 day forecase
         var fiveDayH2El = document.createElement("h2");
         fiveDayH2El.textContent = "5 Day Forecast";
         fiveDayH2El.className = "fiveDayH2";
         weatherContEl.append(fiveDayH2El);
 
+        // fetch 5 day forecast api
         var getFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + getCity + "&units=imperial" + "&appid=" + key;
         fetch(getFiveDay, {
         })
@@ -198,6 +235,7 @@ for (var i = 0; i < getButtons.length; ++i) {
             .then(function (data2) {
                 console.log(data2);
 
+                // crate div container element
                 var fiveDayCont = document.createElement("div");
                 fiveDayCont.className = "fiveDayForecast";
                 
@@ -207,6 +245,7 @@ for (var i = 0; i < getButtons.length; ++i) {
                     var dayContEl = document.createElement("div");
                     dayContEl.className = "dayCont";
 
+                    // create html elements and set content for 5 day
                     var dateEl = document.createElement("h3");
                     dateEl.innerHTML = moment().add(addDay, "d").format("M/DD/YYYY");
 
@@ -216,6 +255,7 @@ for (var i = 0; i < getButtons.length; ++i) {
                     var fiveDayHumidity = document.createElement("p");
                     fiveDayHumidity.innerHTML = "Humidity: " + data2.list[i].main.humidity + "\%";
 
+                    // append elements to div container
                     dayContEl.append(dateEl, fiveDayTemp, fiveDayHumidity);
                     fiveDayCont.append(dayContEl);
                     addDay += 1;
@@ -224,6 +264,7 @@ for (var i = 0; i < getButtons.length; ++i) {
             });  
         }); 
 
+        // append all elements to main content
         todaysForecastEl.append(cityName, tempEl, humidityEl, windSpeedEl, uvEl);
         weatherContEl.append(todaysForecastEl);
         inputEl.value = "";
